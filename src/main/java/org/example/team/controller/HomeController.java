153dafe6b22b0c18f1687e3903a123ec7c0b58b8
hyeokkiyaa <khm38607574@gmail.com>
2024-12-06 17:30:28 +0900

    changes made so direcint allowed

--- src/main/java/org/example/team/controller/HomeController.java
+++ src/main/java/org/example/team/controller/HomeController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@Controller //어노테이션 등록
+@Controller
 public class HomeController {
+
     @RequestMapping("/")
     public String home() {
-        return "index";
+        return "login";
     }
+
 }
