11285bc8244165ba0109839eb6bcc1705c2cf6cf
ìºzionkim <22100130@handong.ac.kr>
2024-12-08 15:08:59 +0900

    lombok - getter, setter

--- src/main/java/org/example/team/vo/MyListVO.java
+++ src/main/java/org/example/team/vo/MyListVO.java
@@ -1,7 +1,12 @@
 package org.example.team.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
 public class MyListVO {
     private int id;
     private String recipe_name;
@@ -18,91 +23,4 @@ public class MyListVO {
     public MyListVO() {
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getRecipe_name() {
-        return recipe_name;
-    }
-
-    public void setRecipe_name(String recipe_name) {
-        this.recipe_name = recipe_name;
-    }
-
-    public String getMain_ingredient() {
-        return main_ingredient;
-    }
-
-    public void setMain_ingredient(String main_ingredient) {
-        this.main_ingredient = main_ingredient;
-    }
-
-    public String getIngredient() {
-        return ingredient;
-    }
-
-    public void setIngredient(String ingredient) {
-        this.ingredient = ingredient;
-    }
-
-    public String getCooking_order() {
-        return cooking_order;
-    }
-
-    public void setCooking_order(String cooking_order) {
-        this.cooking_order = cooking_order;
-    }
-
-    public int getPrep_time() {
-        return prep_time;
-    }
-
-    public void setPrep_time(int prep_time) {
-        this.prep_time = prep_time;
-    }
-
-    public int getDifficulty_level() {
-        return difficulty_level;
-    }
-
-    public void setDifficulty_level(int difficulty_level) {
-        this.difficulty_level = difficulty_level;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public int getCnt() {
-        return cnt;
-    }
-
-    public void setCnt(int cnt) {
-        this.cnt = cnt;
-    }
-
-    public Date getRegdate() {
-        return regdate;
-    }
-
-    public void setRegdate(Date regdate) {
-        this.regdate = regdate;
-    }
 }
--- src/main/java/org/example/team/vo/UserVO.java
+++ src/main/java/org/example/team/vo/UserVO.java
@@ -1,7 +1,12 @@
 package org.example.team.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
 public class UserVO {
     private int id;
     private String userid;
@@ -14,78 +19,6 @@ public class UserVO {
     private String facebook; // 선택사항
 
     public UserVO() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getRegdate() {
-        return regdate;
-    }
-
-    public void setRegdate(Date regdate) {
-        this.regdate = regdate;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public String getInstagram() {
-        return instagram;
-    }
-
-    public void setInstagram(String instagram) {
-        this.instagram = instagram;
-    }
-
-    public String getFacebook() {
-        return facebook;
-    }
-
-    public void setFacebook(String facebook) {
-        this.facebook = facebook;
     }
 
 
